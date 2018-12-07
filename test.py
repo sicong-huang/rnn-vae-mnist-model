b1fbb4da2dd9d5f183ba4e39f5f9ca0fef9cd058
Sicong Huang <hugohuang1997@gmail.com>
2018-12-07 02:08:56 -0800

    working test.py

--- test.py
+++ test.py
@@ -6,13 +6,18 @@ import data_utils
 
 if __name__ == '__main__':
     root = tk.Tk()
-    canvas = tk.Canvas(root)
-    canvas.pack()
-    
-    root.mainloop()
     _, _, x_test, _ = data_utils.load_data()
     img = x_test[0]
-    image=Image.fromarray(img)
+    img = img * 255
+
+    print(img.shape)
+    # height, width = img.shape
+    canvas = tk.Canvas(root)
+    canvas.pack()
+
+    image = Image.fromarray(img).resize((140, 140), Image.ANTIALIAS)
     photo = ImageTk.PhotoImage(image)
 
-    canvas.create_image(photo)
+    canvas.create_image(0, 0, image=photo, anchor=tk.NW)
+
+    root.mainloop()
